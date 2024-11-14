54f7c55370df24cce7709332a80d0a0e70da08e3
Michael Swisa <ms0504109182@gmail.com>
2024-11-14 11:17:15 +0200

    Saving all emails in mongo

---
+++ messages-all-consumer/messages_all_consumer.py
@@ -0,0 +1,22 @@
+from kafka import KafkaConsumer
+import json
+from pymongo import MongoClient
+
+# MongoDB setup
+client = MongoClient('mongodb://mongodb:27017/')
+db = client['all_messages']
+collection = db['all_messages']
+
+# Kafka consumer setup
+consumer = KafkaConsumer(
+    'messages.all',
+    bootstrap_servers=['kafka:9092'],
+    auto_offset_reset='earliest',
+    group_id='messages.all_groups',
+    value_deserializer=lambda m: json.loads(m.decode('utf-8'))
+)
+
+for message in consumer:
+    transaction = message.value
+    collection.insert_one(transaction)
+    print(f"Stored high-value transaction: {transaction}")
